5a7a70dcc5b9f84b02468795c2ca0fba895c951d
Dancho <ddancho@gmail.com>
2022-03-10 15:47:18 +0100

    added maskable icon, monitoring install, activate and fetch events

---
+++ public/serviceWorker.js
@@ -0,0 +1,11 @@
+self.addEventListener("install", (e) => {
+  console.log("service worker installed...");
+});
+
+self.addEventListener("activate", (e) => {
+  console.log("service worker activated....");
+});
+
+self.addEventListener("fetch", (e) => {
+  console.log("fetch event kicked....");
+});
