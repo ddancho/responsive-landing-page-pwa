3dae1e066538ae0fde9892a5c55989879261cf94
Dancho <ddancho@gmail.com>
2022-03-11 15:26:09 +0100

    dynamic caching

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,4 +1,6 @@
 const staticCache = "static-cache-v1";
+const dynamicCache = "dynamic-cache-v1";
+
 const assettsToCache = [
   "/index.html",
   "https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap",
@@ -32,11 +34,26 @@ self.addEventListener("activate", (e) => {
 self.addEventListener("fetch", (e) => {
   console.log("fetch event kicked...");
 
+  // process only http(s) protocols
+  if (!e.request.url.includes("http") || !e.request.url.includes("https")) return;
+
   // check if the requested resource is in the cache
   e.respondWith(
     caches.match(e.request).then((cacheResource) => {
       // return the cache if found or fetch it from the server
-      return cacheResource || fetch(e.request);
+      return (
+        cacheResource ||
+        fetch(e.request).then((fetchResource) => {
+          // cache new resource as dynamic cache
+          return caches.open(dynamicCache).then((cache) => {
+            // fetchResource is stream type
+            // so to save to dynamic cache and to return it to the app, we call clone method on it
+            cache.put(e.request.url, fetchResource.clone());
+            // so we can return it properly
+            return fetchResource;
+          });
+        })
+      );
     })
   );
 });
